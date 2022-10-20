e168da35a20a12a92b2e430ca2f2b903a617421f
Minh Tran <minhtq9700@gmail.com>
2022-10-20 15:24:34 +0700

    ok

--- route/userRoute.js
+++ route/userRoute.js
@@ -4,7 +4,7 @@ const chatBotController = require('../controller/chatBotController');
 
 // Register
 router.get('/', chatBotController.getHomePage);
-router.get('/webhook', chatBotController.getHomePage);
+router.get('/webhook', chatBotController.getWebHook);
 router.post('/webhook', chatBotController.postWebHook);
 
 module.exports = router;
