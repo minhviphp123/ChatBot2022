fc22e3149309cbd9a4dad6a47722d241e6ad6239
Minh Tran <minhtq9700@gmail.com>
2022-11-06 18:54:46 +0700

    ok

--- controller/chatBotController.js
+++ controller/chatBotController.js
@@ -76,7 +76,7 @@ async function handleMessage(sender_psid, received_message) {
     let username
 
     // Checks if the message contains text
-    if (received_message.text === 'hello' || received_message.text === 'hi') {
+    if (received_message.text === 'hello') {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
         // request({
@@ -104,37 +104,37 @@ async function handleMessage(sender_psid, received_message) {
         response = { 'text': `You sent the message: "${received_message.text}". Now send me an image!` }
     }
 
-    if (received_message.attachments) {
-        // Get the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-        // response = {
-        //     "attachment": {
-        //         "type": "template",
-        //         "payload": {
-        //             "template_type": "generic",
-        //             "elements": [{
-        //                 "title": "Is this the right picture?",
-        //                 "subtitle": "Tap a button to answer.",
-        //                 "image_url": attachment_url,
-        //                 "buttons": [
-        //                     {
-        //                         "type": "postback",
-        //                         "title": "Yes!",
-        //                         "payload": "yes",
-        //                     },
-        //                     {
-        //                         "type": "postback",
-        //                         "title": "No!",
-        //                         "payload": "no",
-        //                     }
-        //                 ],
-        //             }]
-        //         }
-        //     }
-        // }
-        response = { 'text': 'attachment' }
-        callSendAPI(sender_psid, response);
-    }
+    // if (received_message.attachments) {
+    //     // Get the URL of the message attachment
+    //     let attachment_url = received_message.attachments[0].payload.url;
+    //     // response = {
+    //     //     "attachment": {
+    //     //         "type": "template",
+    //     //         "payload": {
+    //     //             "template_type": "generic",
+    //     //             "elements": [{
+    //     //                 "title": "Is this the right picture?",
+    //     //                 "subtitle": "Tap a button to answer.",
+    //     //                 "image_url": attachment_url,
+    //     //                 "buttons": [
+    //     //                     {
+    //     //                         "type": "postback",
+    //     //                         "title": "Yes!",
+    //     //                         "payload": "yes",
+    //     //                     },
+    //     //                     {
+    //     //                         "type": "postback",
+    //     //                         "title": "No!",
+    //     //                         "payload": "no",
+    //     //                     }
+    //     //                 ],
+    //     //             }]
+    //     //         }
+    //     //     }
+    //     // }
+    //     response = { 'text': 'attachment' }
+    //     callSendAPI(sender_psid, response);
+    // }
 
     // Send the response message
 
