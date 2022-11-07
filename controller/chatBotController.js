c599963253f245c2558e018edde9033e7a16ea76
Minh Tran <minhtq9700@gmail.com>
2022-11-07 15:54:18 +0700

    ok

--- controller/chatBotController.js
+++ controller/chatBotController.js
@@ -140,7 +140,7 @@ async function handleMessage(sender_psid, received_message) {
 
 }
 
-function callSendAPI(sender_psid, response) {
+async function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
         "recipient": {
@@ -148,6 +148,10 @@ function callSendAPI(sender_psid, response) {
         },
         "message": response
     }
+
+    await sendMarkSeen(sender_psid);
+
+    await sendTypingOn(sender_psid);
 
     // Send the HTTP request to the Messenger Platform
     request({
@@ -160,6 +164,54 @@ function callSendAPI(sender_psid, response) {
             console.log('message sent!')
         } else {
             console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+function sendTypingOn(sender_psid) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "sender_action": "mark_seen"
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('mark_seen!')
+        } else {
+            console.error("Unable to send m_s:" + err);
+        }
+    });
+}
+
+function sendMarkSeen(sender_psid) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "sender_action": "typing_on"
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('typing_on!')
+        } else {
+            console.error("Unable to send t_o:" + err);
         }
     });
 }
